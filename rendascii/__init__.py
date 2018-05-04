095d59b081f43a6156f7e4a08382ddffdc27c05d
Garrett Fairburn <breadboardfox@gmail.com>
2018-05-04 14:35:45 -0400

    Added error handling for versioning.

--- rendascii/__init__.py
+++ rendascii/__init__.py
@@ -1,4 +1,6 @@
-from pkg_resources import get_distribution
+from pkg_resources import get_distribution, DistributionNotFound
 
-
-__version__ = get_distribution(__name__).version
+try:
+  __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+  pass
