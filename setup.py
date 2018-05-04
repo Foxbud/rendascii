8839931c9db088c5c1f2c0dc216330d0db4329be
Garrett Fairburn <breadboardfox@gmail.com>
2018-05-04 14:08:04 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rendascii',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    description='ASCII 3D rendering engine',
+    url='https://bitbucket.org/foxbudpersonal/rendascii',
+    author='Garrett Fairburn',
+    author_email='breadboardfox@gmail.com',
+    license='MIT',
+    packages=['rendascii']
+)
