c50cf4a8adef0ca40c61de248aa5248696b98048
Garrett Fairburn <breadboardfox@gmail.com>
2018-05-12 17:51:43 -0400

    Started implementing Model class in resource.

--- rendascii/resource.py
+++ rendascii/resource.py
@@ -5,5 +5,9 @@ TBA.
 
 class Model:
 
-  def __init__(self):
-    pass
+  def __init__(self, file_name):
+    # Initialize instance attributes.
+    self.vertices = []
+    self.faces = []
+    self.face_normals = []
+    self.face_colors = []
