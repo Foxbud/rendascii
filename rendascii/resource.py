f4bab1f46dbadf2b6c01fc7d4d106b3406e2c36a
Garrett Fairburn <breadboardfox@gmail.com>
2018-05-12 22:33:59 -0400

    Implemented material file loading in Model class of resource module.

--- rendascii/resource.py
+++ rendascii/resource.py
@@ -11,3 +11,22 @@ class Model:
     self.faces = []
     self.face_normals = []
     self.face_colors = []
+
+    # Load materials from file.
+    materials = {}
+    with open(file_name + '.mtl', 'r') as mtl_f:
+      cur_mtl = None
+      for line in mtl_f:
+        words = line.split()
+        # Check for new material.
+        if words[0] == 'newmtl':
+          cur_mtl = words[1]
+        # Check for material color (diffuse).
+        elif words[1] == 'Kd':
+          materials[cur_mtl] = ''.join(
+              [
+                hex(round(float(component)))[2:]
+                for component
+                in words[1:]
+                ]
+              )
