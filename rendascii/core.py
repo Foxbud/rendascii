e9f636d2b7b2bc6508802fb44370aa4d805a4d21
Garrett Fairburn <breadboardfox@gmail.com>
2018-05-15 00:46:07 -0400

    Started implementing Camera class of core module.

--- rendascii/core.py
+++ rendascii/core.py
@@ -3,7 +3,30 @@ TBA.
 """
 
 
+from rendascii.geometry import Vec2D
+from rendascii.geometry import Vec3D
+
+
 class Camera:
 
-  def __init__(self):
-    pass
+  def __init__(self, width, height, num_pixels_x, num_pixels_y, focal_dist):
+    # Initialize instance attributes.
+    self.max_bound = Vec2D([width / 2, height / 2])
+    self.min_bound = Vec2D(self.max_bound)
+    self.min_bound.neg()
+    frag_size = Vec2D([width / num_pixels_x, height / num_pixels_y])
+    self.fragments = [
+        [
+          Vec2D(
+            [
+              self.min_bound.x + frag_size.x * (x + 0.5),
+              self.min_bound.y + frag_size.y * (y + 0.5)
+              ]
+            )
+          for x
+          in range(num_pixels_x)
+          ]
+        for y
+        in range(num_pixels_y)
+        ]
+    self.focal_point = Vec3D([0, 0, -focal_dist])
