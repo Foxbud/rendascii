6d4299eaaa25f0d976a0fb951cf4099dd623ddfd
Garrett Fairburn <breadboardfox@gmail.com>
2018-05-20 21:10:29 -0400

    Created data for rendering pipeline.

--- rendascii/interface.py
+++ rendascii/interface.py
@@ -4,17 +4,58 @@ TBA.
 
 
 from rendascii.geometry import matrix3d
+from rendascii.geometry import X, Y
+from rendascii.resources import generate_camera_fragments
 
 
 class Engine:
 
-  def __init__(self):
+  def __init__(self, camera_scale, camera_resolution):
     # Initialize instance attributes.
+    # Camera.
+    self._camera_scale = camera_scale
+    self._camera_orientation = (0.0, 0.0, 0.0)
+    self._camera_angle_order = 'xzy'
+    self._camera_rot_matrix = matrix3d.generate_rotation_matrix(
+        self._camera_orientation,
+        self._camera_angle_order
+        )
+    self._camera_position = (0.0, 0.0, 0.0)
+    self._camera_fragments = generate_camera_fragments(
+        camera_scale[X],
+        camera_scale[Y],
+        camera_resolution[X],
+        camera_resolution[Y]
+        )
+    # Static.
     self._models = {}
+    self._model_instances = []
+    # Pipeline - Stage 1.
     self._verts_model_space = []
+    self._vert_world_scales = []
+    self._vert_world_orientations = []
+    self._vert_world_positions = []
+    self._polygons_world_space = []
+    self._textures_world_space = []
+    self._normals_world_space = []
+    self._normal_world_orientations = []
+    # Pipeline - Stage 2.
     self._verts_world_space = []
+    self._vert_camera_offsets = []
+    self._vert_camera_orientations = []
+    self._normals_world_space = []
+    self._normal_camera_orientations = []
+    # Pipeline - Stage 2.
     self._verts_camera_space = []
+    self._normals_camera_space = []
+    # Pipeline - Stage 3.
     self._verts_projected = []
+    self._vert_depths = []
+    self._polygons_projected = []
+    self._textures_projected = []
+    self._aabbs_projected = []
+    # Pipeline - Stage 4.
+    self._pixel_fragments
 
 
 class ModelInstance:
