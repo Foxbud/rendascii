bb553f420248c16084fa33f08df403e9172f608c
Garrett Fairburn <breadboardfox@gmail.com>
2018-05-28 20:57:33 -0400

    Added support for extension compilation.

--- setup.py
+++ setup.py
@@ -1,17 +1,70 @@
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import Extension, find_packages, setup
 
 
 setup(
     name='rendascii',
     use_scm_version=True,
-    setup_requires=['setuptools_scm',],
+    setup_requires=['setuptools_scm', 'cython',],
+    ext_modules=[
+      Extension(
+        'rendascii.__init__',
+        sources=['rendascii/__init__.py',]
+        ),
+      Extension(
+        'rendascii.interface',
+        sources=['rendascii/interface.py',]
+        ),
+      Extension(
+        'rendascii.resource',
+        sources=['rendascii/resource.py',]
+        ),
+      Extension(
+        'rendascii.pipeline.__init__',
+        sources=['rendascii/pipeline/__init__.py',]
+        ),
+      Extension(
+        'rendascii.pipeline.shader',
+        sources=['rendascii/pipeline/shader.py',]
+        ),
+      Extension(
+        'rendascii.pipeline.stage',
+        sources=['rendascii/pipeline/stage.py',]
+        ),
+      Extension(
+        'rendascii.geometry.__init__',
+        sources=['rendascii/geometry/__init__.py',]
+        ),
+      Extension(
+        'rendascii.geometry.matrix',
+        sources=['rendascii/geometry/matrix.py',]
+        ),
+      Extension(
+        'rendascii.geometry.poly2d',
+        sources=['rendascii/geometry/poly2d.py',]
+        ),
+      Extension(
+        'rendascii.geometry.poly3d',
+        sources=['rendascii/geometry/poly3d.py',]
+        ),
+      Extension(
+        'rendascii.geometry.vec2d',
+        sources=['rendascii/geometry/vec2d.py',]
+        ),
+      Extension(
+        'rendascii.geometry.vec3d',
+        sources=['rendascii/geometry/vec3d.py',]
+        ),
+      Extension(
+        'rendascii.geometry.vech',
+        sources=['rendascii/geometry/vech.py',]
+        ),
+      ],
     description='ASCII 3D rendering engine',
     url='https://bitbucket.org/foxbudpersonal/rendascii',
     author='Garrett Fairburn',
     author_email='breadboardfox@gmail.com',
     license='MIT',
     packages=find_packages(),
-    package_data={'': ['*.so',],},
-    include_package_data=True
+    #package_data={'': ['*.so',],},
+    #include_package_data=True
 )
