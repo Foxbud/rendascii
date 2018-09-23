2a92f149d3515925747c860da4b65e103a5bbf10
Foxbud <breadboardfox@gmail.com>
2018-09-23 02:30:35 -0400

    Reworked setup script and started writing README.

---
+++ README.md
@@ -0,0 +1,19 @@
+# RendASCII
+
+**RendASCII** is a real-time 3D rendering engine for Python 3. Rather than displaying rendered frames in a graphical window, **RendASCII** renders frames to printable strings using ASCII characters for pixels.
+
+[Project Homepage](https://bitbucket.org/Foxbud/rendascii)
+
+[Project Documentation](https://bitbucket.org/Foxbud/rendascii/wiki)
+
+## Concepts
+
+## API
+
+`rendascii.interface.Engine`
+
+`rendascii.utility.FrameRateManager`
+
+`rendascii.utility.Transformer`
+
+## FAQ
--- setup.py
+++ setup.py
@@ -4,69 +4,125 @@ See file LICENSE.txt for full license details.
 """
 
 
-from Cython.Build import cythonize
-from setuptools import Extension, find_packages, setup
+from platform import python_implementation
+from setuptools import find_packages, setup
 
 
-ext_modules = [
-    Extension(
-      'rendascii.interface',
-      sources=['rendascii/interface.py',],
-      extra_compile_args=['-O1',]
-      ),
-    Extension(
-      'rendascii.resource',
-      sources=['rendascii/resource.py',],
-      extra_compile_args=['-O1',]
-      ),
-    Extension(
-      'rendascii.utility',
-      sources=['rendascii/utility.py',],
-      extra_compile_args=['-O1',]
-      ),
-    Extension(
-      'rendascii.pipeline.shader',
-      sources=['rendascii/pipeline/shader.py',],
-      extra_compile_args=['-O1',]
-      ),
-    Extension(
-      'rendascii.pipeline.stage',
-      sources=['rendascii/pipeline/stage.py',],
-      extra_compile_args=['-O1',]
-      ),
-    Extension(
-      'rendascii.geometry.matrix',
-      sources=['rendascii/geometry/matrix.py',],
-      extra_compile_args=['-O1',]
-      ),
-    Extension(
-      'rendascii.geometry.polygon',
-      sources=['rendascii/geometry/polygon.py',],
-      extra_compile_args=['-O1',]
-      ),
-    Extension(
-      'rendascii.geometry.vector',
-      sources=['rendascii/geometry/vector.py',],
-      extra_compile_args=['-O1',]
-      ),
+# Package parameters.
+name = 'rendascii'
+use_scm_version = True
+setup_requires = ['setuptools_scm',]
+description = 'ASCII 3D rendering engine'
+with open('README.md', 'r') as f_in:
+  long_description = f_in.read()
+long_description_content_type = 'text/markdown'
+url = 'https://bitbucket.org/Foxbud/rendascii'
+author = 'Garrett Fairburn'
+author_email = 'breadboardfox@gmail.com'
+license = 'MIT'
+packages = find_packages()
+classifiers = [
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Environment :: Console',
+    'Topic :: Multimedia :: Graphics :: 3D Rendering',
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
     ]
 
 
-setup(
-    name='rendascii',
-    use_scm_version=True,
-    ext_modules=cythonize(
-      ext_modules,
-      compiler_directives={
-        'embedsignature': True,
-        }
-      ),
-    setup_requires=['setuptools_scm', 'cython',],
-    description='ASCII 3D rendering engine',
-    url='https://bitbucket.org/foxbudpersonal/rendascii',
-    author='Garrett Fairburn',
-    author_email='breadboardfox@gmail.com',
-    license='MIT',
-    packages=find_packages(),
-    data_files = [("", ["LICENSE.txt"])]
-)
+# Build without extensions.
+def build_wo_ext():
+  setup(
+      name=name,
+      use_scm_version=use_scm_version,
+      setup_requires=setup_requires,
+      description=description,
+      long_description=long_description,
+      long_description_content_type=long_description_content_type,
+      url=url,
+      author=author,
+      author_email=author_email,
+      license=license,
+      packages=packages,
+      classifiers=classifiers
+  )
+
+
+# Build with extensions.
+def build_w_ext():
+  from Cython.Build import cythonize
+  from setuptools import Extension
+  
+  ext_modules = [
+      Extension(
+        'rendascii.interface',
+        sources=['rendascii/interface.py',],
+        extra_compile_args=['-O1',]
+        ),
+      Extension(
+        'rendascii.resource',
+        sources=['rendascii/resource.py',],
+        extra_compile_args=['-O1',]
+        ),
+      Extension(
+        'rendascii.utility',
+        sources=['rendascii/utility.py',],
+        extra_compile_args=['-O1',]
+        ),
+      Extension(
+        'rendascii.pipeline.shader',
+        sources=['rendascii/pipeline/shader.py',],
+        extra_compile_args=['-O1',]
+        ),
+      Extension(
+        'rendascii.pipeline.stage',
+        sources=['rendascii/pipeline/stage.py',],
+        extra_compile_args=['-O1',]
+        ),
+      Extension(
+        'rendascii.geometry.matrix',
+        sources=['rendascii/geometry/matrix.py',],
+        extra_compile_args=['-O1',]
+        ),
+      Extension(
+        'rendascii.geometry.polygon',
+        sources=['rendascii/geometry/polygon.py',],
+        extra_compile_args=['-O1',]
+        ),
+      Extension(
+        'rendascii.geometry.vector',
+        sources=['rendascii/geometry/vector.py',],
+        extra_compile_args=['-O1',]
+        ),
+      ]
+  
+  setup(
+      name=name,
+      use_scm_version=use_scm_version,
+      ext_modules=cythonize(
+        ext_modules,
+        compiler_directives={
+          'embedsignature': True,
+          }
+        ),
+      setup_requires=setup_requires + ['cython',],
+      description=description,
+      long_description=long_description,
+      long_description_content_type=long_description_content_type,
+      url=url,
+      author=author,
+      author_email=author_email,
+      license=license,
+      packages=packages,
+      classifiers=classifiers
+  )
+
+
+# Only build extension modules if using CPython.
+if python_implementation() == 'CPython':
+  build_w_ext()
+else:
+  build_wo_ext()
